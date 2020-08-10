6cc20d1677492c10808c101d01f632ccde35af77
sidgroks@gmail.com <sidgroks@gmail.com>
2020-08-10 18:57:58 +0530

    base router flux addition

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,101 +8,46 @@ import {
   View,
   Text,
   StatusBar,
+  Platform,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Scene, Router, Actions, Stack} from 'react-native-router-flux';
 
-const App: () => React$Node = () => {
+import {Home} from './components';
+
+const App = () => {
+  useEffect(() => {
+    return Actions.home();
+  }, []);
+
+  const stateHandler = (prevState, newState, action) => {
+    console.log('onStateChange: ACTION:', action);
+  };
+
+  const prefix = Platform.OS === 'android' ? 'mychat://mychat/' : 'mychat://';
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <Router
+        onStateChange={stateHandler}
+        sceneStyle={styles.scene}
+        uriPrefix={prefix}>
+        <Stack key="root">
+          <Scene
+            key="home"
+            component={Home}
+            title="Home"
+            onEnter={() => console.log('Home: onEnter')}
+            backTitle="Back"
+            panHandlers={null}
+            duration={1}
+          />
+        </Stack>
+      </Router>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
---
+++ components/Home.js
@@ -0,0 +1,9 @@
+import {View} from 'react-native';
+
+export default function Home(props) {
+  return (
+    <>
+      <View>Hi</View>
+    </>
+  );
+}
