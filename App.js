b134b84d90eb3413f1d996762802cd0ee6b62d30
sidgroks@gmail.com <sidgroks@gmail.com>
2020-08-11 11:38:38 +0530

    react native elements addition and example login screen

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import 'react-native-gesture-handler';
 
 import React, {useEffect} from 'react';
+import {ThemeProvider} from 'react-native-elements';
 import {StyleSheet, Platform} from 'react-native';
 
 import {Scene, Router, Actions, Stack} from 'react-native-router-flux';
 
-import {Home} from './components';
+import {Home, Login} from './screens';
 
 export default function App() {
-  // useEffect(() => {
-  // return Actions.home();
-  // }, []);
-
   const stateHandler = (prevState, newState, action) => {
     console.log('onStateChange: ACTION:', action);
   };
@@ -20,23 +17,34 @@ export default function App() {
 
   return (
     <>
-      <Router
-        onStateChange={stateHandler}
-        sceneStyle={styles.scene}
-        uriPrefix={prefix}>
-        <Stack key="root">
-          <Scene
-            key="home"
-            initial
-            component={Home}
-            title="Home"
-            onEnter={() => console.log('Home: onEnter')}
-            backTitle="Back"
-            panHandlers={null}
-            duration={1}
-          />
-        </Stack>
-      </Router>
+      <ThemeProvider>
+        <Router
+          onStateChange={stateHandler}
+          sceneStyle={styles.scene}
+          uriPrefix={prefix}>
+          <Stack key="root">
+            <Scene
+              key="login"
+              initial
+              component={Login}
+              title="Login"
+              onEnter={() => console.log('Login: onEnter')}
+              backTitle="Back"
+              panHandlers={null}
+              duration={1}
+            />
+            <Scene
+              key="home"
+              component={Home}
+              title="Home"
+              onEnter={() => console.log('Home: onEnter')}
+              backTitle="Back"
+              panHandlers={null}
+              duration={1}
+            />
+          </Stack>
+        </Router>
+      </ThemeProvider>
     </>
   );
 }
---
+++ screens/Login.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {Input, Button} from 'react-native-elements';
+import {Actions} from 'react-native-router-flux';
+import Icons from 'react-native-vector-icons';
+
+export default function Login(props) {
+  const handleLogin = () => {
+    Actions.home();
+  };
+
+  return (
+    <>
+      <View>
+        <Input
+          placeholder="Email"
+          leftIcon={{
+            type: 'feathericons',
+            name: 'mail',
+          }}></Input>
+        <Input
+          placeholder="Password"
+          type="password"
+          leftIcon={{
+            type: 'feathericons',
+            name: 'lock',
+          }}></Input>
+        <Button
+          onPress={handleLogin}
+          title="Login"
+          iconRight={{
+            name: 'chevron-right',
+            size: 15,
+            color: 'white',
+          }}
+        />
+      </View>
+    </>
+  );
+}
